8acc39e362f2d44ac97721154f4672eb6f979141
duvan hurtado <duanhurtado@gmail.com>
2025-08-28 18:32:25 -0500

    logging log4j

---
+++ loggin/src/main/java/com/log4j/Gerente.java
@@ -0,0 +1,19 @@
+package com.log4j;
+
+public class Gerente extends Empleado {
+
+    private String departamento;
+
+    public Gerente(String nombre, int edad, double salario, String departamento) {
+        super(nombre, edad, salario);
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Gerente{" +
+                "departamento='" + departamento + '\'' +
+                "} " + super.toString();
+    }
+
+}
